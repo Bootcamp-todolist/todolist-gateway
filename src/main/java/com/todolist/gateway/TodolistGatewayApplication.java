0fc4fa43869bb23714bbf56ea25eda2f7b377b51
kexin.an <kexin.an@thoughtworks.com>
2023-08-14 11:06:12 +0800

    init

---
+++ src/main/java/com/todolist/gateway/TodolistGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodolistGatewayApplication.class, args);
+  }
+
+}
